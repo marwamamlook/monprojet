26ed84ad628de7f20446d59968d00b1d4d75b4eb
marwamamlouk <marwamamlouk12@gmail.com>
2022-06-11 15:45:24 +0100

    edit2

--- resources/views/retraites/index.blade.php
+++ resources/views/retraites/index.blade.php
@@ -17,8 +17,7 @@
                         <!-- -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Accueil</a></li>
-                            <li class="breadcrumb-item active">Retraites</li>
+                            <li class="breadcrumb-item active"></li>
                             </ol>
                         </div>
                         </div>
@@ -29,7 +28,7 @@
                         <div class="col-md-12">
                         <div class="card">
                             <div class="card-header">
-                            <h3 class="card-title">DataTable with minimal features &amp; hover style</h3>
+                            <h3 class="card-title">Liste des retraites</h3>
                             </div>
                         <!-- /.card-header -->
                         <div class="card-body">
